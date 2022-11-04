d2added522d88130d8568b0a4782fe31ffb62074
Harshit Joshi <59091077+Harshitjoc@users.noreply.github.com>
2022-11-04 22:49:12 +0530

    Create String Split and Join.py

---
+++ String Split and Join.py
@@ -0,0 +1,12 @@
+#hackerrank python question
+
+def split_and_join(line):
+    line = line.split(' ')
+    line = "-".join(line)
+    return line
+    # write your code here
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
