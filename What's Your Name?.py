ba56208246104fe60459928b589a89956ba34610
Harshit Joshi <59091077+Harshitjoc@users.noreply.github.com>
2022-11-05 21:46:01 +0530

    Create What's Your Name?.py

---
+++ What's Your Name?.py
@@ -0,0 +1,12 @@
+#hacker rank python question
+
+def print_full_name(first, last):
+    String = 'Hello {} {}! You just delved into python.'.format(first, last)
+    print(String)
+
+    # Write your code here
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
